1a726ec3d0c44628845baad3319062fc5cbca1c0
aparus <rustamapaev@gmail.com>
2021-01-21 23:18:20 +0300

    added isLoading state for SeachInput, refactored

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
 import InputBase from '@material-ui/core/InputBase'
@@ -6,6 +6,7 @@ import Divider from '@material-ui/core/Divider'
 import IconButton from '@material-ui/core/IconButton'
 import ClearIcon from '@material-ui/icons/Clear'
 import SearchIcon from '@material-ui/icons/Search'
+import LinearProgress from '@material-ui/core/LinearProgress'
 import ytdl from 'ytdl-core'
 import { useRouter } from 'next/router'
 
@@ -26,12 +27,18 @@ const useStyles = makeStyles(theme => ({
 	divider: {
 		height: 28,
 		margin: 4
+	},
+	loadingProgressContainer: {
+		textAlign: 'center',
+		marginTop: 10,
+		width: '100%'
 	}
 }))
 
 export default function CustomizedInputBase() {
 	const classes = useStyles()
 	const [url, setUrl] = useState('')
+	const [isLoading, setIsLoading] = useState()
 	const router = useRouter()
 
 	const onSearchChange = event => {
@@ -52,8 +59,29 @@ export default function CustomizedInputBase() {
 		}
 	}
 
-	return (
-		<Paper component='form' onSubmit={onSubmit} className={classes.root}>
+	useEffect(() => {
+		//possible params: (url, { shallow })
+		const handleRouteChangeStart = () => setIsLoading(true)
+		const handleRouteChangeComplete = () => setIsLoading(false)
+
+		router.events.on('routeChangeStart', handleRouteChangeStart)
+		router.events.on('routeChangeComplete', handleRouteChangeComplete)
+
+		return () => {
+			router.events.off('routeChangeStart', handleRouteChangeStart)
+			router.events.off('routeChangeComplete', handleRouteChangeComplete)
+		}
+	}, [])
+
+	const LoadingIndicator = () => (
+		<div className={classes.loadingProgressContainer}>
+			<LinearProgress />
+		</div>
+	)
+
+	const SearchInput = () => (
+		<>
+			{' '}
 			<IconButton
 				className={classes.iconButton}
 				aria-label='search'
@@ -76,6 +104,14 @@ export default function CustomizedInputBase() {
 			>
 				<ClearIcon />
 			</IconButton>
-		</Paper>
+		</>
+	)
+
+	return (
+		<div>
+			<Paper component='form' onSubmit={onSubmit} className={classes.root}>
+				{isLoading ? <LoadingIndicator /> : <SearchInput />}
+			</Paper>
+		</div>
 	)
 }
